49a06e1c0a265e203da1d9346a8296b55b9f018c
Chris Maxwell <chmaxwel@icloud.com>
2020-09-03 22:34:15 +0200

    changes to buttons and seeds

--- seeds/comment.js
+++ seeds/comment.js
@@ -1,46 +1,10 @@
 const { Comment } = require('../models');
 
 const commentData = [{
-        user_id: 1,
+    user_id: 1,
 
-        comment_text: "Technology blog"
-    }
-    // {
-    //     user_id: 4,
-
-    //     comment_text: "Super sweet and bubbly. Fresh crisp fruity taste. Very light and refreshing!"
-    // },
-    // {
-    //     user_id: 1,
-
-    //     comment_text: "The best wine of the world! Easy to open, sweet, it’s like a desert!"
-    // },
-    // {
-    //     user_id: 3,
-
-    //     comment_text: "This is hands down my favorite wine ever. I love the taste, the packaging is gorgeous and it's relatively cheap!"
-    // },
-    // {
-    //     user_id: 3,
-
-    //     comment_text: "Tasty, sweet, refreshing and cheap. This is a great choice if you like a fruity wine....no dryness."
-    // },
-    // {
-    //     user_id: 3,
-
-    //     comment_text: "It's really crisp and sweet with notes of citrus and apricot. It's very pleasant and easy to drink wine that goes with pretty much anything"
-    // },
-    // {
-    //     user_id: 5,
-
-    //     comment_text: "Wonderful in the summer in a Sangria with fresh berries and a little fresh mint. So refreshing, reasonably priced!"
-    // },
-    // {
-    //     user_id: 2,
-
-    //     comment_text: "Best white wine. Taste great and crisp and sweet. Perfect blend of Fruits!"
-    // }
-];
+    comment_text: "Technology blog"
+}];
 
 const seedComments = () => Comment.bulkCreate(commentData);
 
